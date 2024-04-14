29a0f587be7d26c9e875c9be9b4f924152b97c4e
Kaktus200020 <artemkremnev2007@gmail.com>
2024-04-14 20:50:27 +0300

    Init

---
+++ TbotTinkoff/Classes/Bot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TbotTinkoff.Classes
+{
+    public class Bot
+    {
+        private TelegramBotClient _bot;
+        private CancellationTokenSource _token;
+
+        public Bot(string token)
+        {
+            _bot = new TelegramBotClient(token);
+            _token = new CancellationTokenSource();
+        }
+        public void Start()
+        {
+            _bot.StartReceiving(HaldeUpdateAsync, HaldeErrorAsync, new Telegram.Bot.Polling.ReceiverOptions()
+            {
+                ThrowPendingUpdates=true
+            },cancellationToken:_token.Token);
+        }
+        public void Stop() 
+        {
+            _token.Cancel();
+        }
+        public async Task GetInfo()
+        {
+            var botInfo= await _bot.GetMeAsync(cancellationToken: _token.Token);
+            Console.WriteLine($"Бот {botInfo.Username} запущен");
+        }
+        private async Task HaldeUpdateAsync(ITelegramBotClient bot, Update update,CancellationToken cancellationToken)
+        {
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await _bot.SendTextMessageAsync(update.Message.Chat.Id,update.Message.Text,cancellationToken:cancellationToken
+                    );
+            }
+        }
+        private Task HaldeErrorAsync(ITelegramBotClient bot,Exception exception,  CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Error: \n"+exception);
+            Environment.Exit(1);
+            return null;
+
+        }
+    }
+}
---
+++ TbotTinkoff/Program.cs
@@ -0,0 +1,14 @@
+﻿using TbotTinkoff.Classes;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var bot = new Bot("7104333258:AAE6n2a8kFA67-cBrUVt_Z-z6rbmieExmzM");
+
+        bot.Start();
+        await bot.GetInfo();
+        Console.ReadLine();
+        bot.Stop();
+    }
+}
